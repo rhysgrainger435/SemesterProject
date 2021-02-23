e5368b93c0d1a1a012885642c399a02c5587fa7e
Aaron Kenny <X00166550@mytudublin.ie>
2021-02-23 17:44:15 +0000

    more errors

--- phoneshop/urls.py
+++ phoneshop/urls.py
@@ -5,6 +5,6 @@ app_name ='phoneshop'
 
 urlpatterns = [
     path('', views.allProdCat, name ='allProdCat'),
-    path('<uuid:category_id>/', views.allProdCat, name ='products_by_category'),
+    path('<uuid:category_id>/', views.allProdCat, name='products_by_category'),
     path('<uuid:cateogry_id>/<uuid:product_id>/', views.prod_detail, name='prod_detail'),
 ]
--- phoneshop/views.py
+++ phoneshop/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Category, Product
+from .models import Category,Product
 
 def allProdCat(request, category_id=None):
     c_page = None
@@ -11,11 +11,11 @@ def allProdCat(request, category_id=None):
     else:
         products = Product.objects.all().filter(available=True)
 
-    return render(request, 'phoneshop/category.html', {'category':c_page, 'products':products})
+    return render(request,'shop/category.html',{'category':c_page, 'products':products})
 
-def prod_detail(request, category_id, product_id):
+def prod_detail(request,category_id,product_id):
     try:
-        product = Product.objects.get(category_id=category_id, id=product_id)
+        product = Product.objects.get(category_id=category_id,id=product_id)
     except Exception as e:
         raise e 
-    return render(request, 'phoneshop/product.html', {'product':product})
+    return render(request,'shop/product.html', {'product':product})
